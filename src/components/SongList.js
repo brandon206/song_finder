5d4c0f5b826baca36eecb370f83fb86ecae18b91
Brandon Park <brandonpark206@gmail.com>
2018-12-27 20:27:52 -0800

    got mapStateToProps, connect, and render to show the list of songs on the DOM

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+
+    renderList() {
+        return this.props.songs.map( (song) => {
+            return (
+                <div className="item" key = {song.title}>
+                    <div className = "row">
+                        <button className = "blue" >Select</button>
+                        <div className = "col s6">{song.title}</div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render () {
-        return <div>SongList</div>
+        //this.props === { songs: state.songs }
+        return <div>{this.renderList()}</div>
     }
 }
 
+const mapStateToProps = (state) => {
+    return { songs: state.songs };
+}
 
-
-export default connect(null,{
-    
-})(SongList);
+export default connect(mapStateToProps)(SongList);
--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,8 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize';
 import React from 'react';
 import SongList from './SongList';
+
 
 const App = () => (
     <div>
